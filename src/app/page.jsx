18501848a2997d078dac5bc928b494dacf34ad94
emmanuel687 <koechemmanuel2002@gmail.com>
2025-09-12 16:24:19 +0300

    Feat:-Added Dark Mode

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,23 +8,10 @@ import { useState } from 'react';
 import { Dropdown } from 'primereact/dropdown';
 
 const Home = () => {
-
-  const [selectedCity, setSelectedCity] = useState(null);
-  const cities = [
-    { name: 'New York', code: 'NY' },
-    { name: 'Rome', code: 'RM' },
-    { name: 'London', code: 'LDN' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Paris', code: 'PRS' }
-  ];
-
   return (
-      <div className='text-bold text-green-600'>Home
-        <div className="card flex justify-content-center">
-          <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name"
-            placeholder="Select a City" className="w-full md:w-14rem" />
-        </div>
-      </div>
+    <div className="p-4">
+   
+    </div>
   )
 }
 
