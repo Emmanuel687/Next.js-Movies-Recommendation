e15fe595394a70d1369ea2724c94276b35183e5c
emmanuel687 <koechemmanuel2002@gmail.com>
2025-09-13 13:50:42 +0300

    Feat:-Intergrated Clerk Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import Header from "./components/Header";
 import Providers from "./components/Theme";
 import { PrimeReactProvider } from "primereact/api";
+import { ClerkProvider } from "@clerk/nextjs";
 import "primereact/resources/primereact.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "./globals.css";
@@ -13,17 +14,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				suppressHydrationWarning
-			>
-				<Providers>
-					<PrimeReactProvider>
-						<Header />
-						<main>{children}</main>
-					</PrimeReactProvider>
-				</Providers>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body suppressHydrationWarning>
+					<Providers>
+						<PrimeReactProvider>
+							<Header />
+							<main>{children}</main>
+						</PrimeReactProvider>
+					</Providers>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
